40f8c1c145da6ad3215d8d6490977a72a48ee7f9
Filip Turek <filippturek@gmail.com>
2018-04-03 20:48:45 +0200

    init commit

---
+++ src/main/java/uj/jwzp/w4/launchers/SpringMain.java
@@ -0,0 +1,35 @@
+package uj.jwzp.w4.launchers;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.core.env.CommandLinePropertySource;
+import org.springframework.core.env.SimpleCommandLinePropertySource;
+import uj.jwzp.w4.logic.CSVMovieFinder;
+import uj.jwzp.w4.logic.MovieLister;
+import uj.jwzp.w4.model.Movie;
+
+@Slf4j
+public class SpringMain {
+
+    public static void main(String[] args) {
+
+        ApplicationContext ctx = configurate(args);
+        MovieLister lister = (MovieLister) ctx.getBean("movieLister");
+
+        lister.moviesDirectedBy("Hoffman").stream()
+                .map(Movie::toString)
+                .forEach(log::info);
+    }
+
+    public static AnnotationConfigApplicationContext configurate (String ... args) {
+        CommandLinePropertySource commandLinePropertySource = new SimpleCommandLinePropertySource(args);
+        AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext();
+        ctx.scan("uj.jwzp.w4.logic");
+
+        ctx.getEnvironment().getPropertySources().addFirst(commandLinePropertySource);
+        ctx.refresh();
+
+        return ctx;
+    }
+}
